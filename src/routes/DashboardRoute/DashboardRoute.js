01481a7c8cc5fb33f53fb4024676ac150eaeb4f7
Michael Bramble <mike.w.bramble@gmail.com>
2019-04-25 11:25:13 -0400

    added temp styles to dashboard page

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './DashboardRoute.css';
 
 class DashboardRoute extends Component {
   render() {
     return (
-      <section>
-        <Link to='/meals'>Create, view, or edit your meals </Link>
-        <Link to='/log'>Log a meal, snack, or exercise</Link>
-        <Link to='/reports'> View your past reports </Link>
+      <section className="dashboard">
+        <button type="button">
+          <Link to='/meals' style={{textDecoration: 'none'}}>
+            Create, View, or Edit Your Meals
+          </Link>
+        </button>
+        <br />
+        <button type="button">
+          <Link to='/log' style={{textDecoration: 'none'}}>
+            Log a Meal, Snack, or Exercise
+          </Link>
+        </button>
+        <br />
+        <button type="button">
+          <Link to='/reports' style={{textDecoration: 'none'}}>
+            View Your Past Reports
+          </Link>
+        </button>
       </section>
     );
-  }
-}
+  };
+};
 
-export default DashboardRoute
+export default DashboardRoute;
