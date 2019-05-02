aff7a0ed346811436a2e3621bbd667b10de5b45d
Michael Bramble <mike.w.bramble@gmail.com>
2019-05-02 16:01:12 -0400

    preparing for merge

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Button from '../Button/Button';
 import UserContext from '../../contexts/UserContext';
 import './Profile.css';
 
-class Profile extends Component {
+export default class Profile extends Component {
   static contextType = UserContext;
 
   state = {
@@ -19,6 +20,29 @@ class Profile extends Component {
     }
   }
 
+  submitUserBudgets = () => {
+    let calorieBudget = Number(document.getElementById("calorieBudget").value)
+      ? Number(document.getElementById("calorieBudget").value)
+      : null;
+    let fatBudget = Number(document.getElementById("fatBudget").value)
+      ? Number(document.getElementById("fatBudget").value)
+      : null;
+    let carbBudget = Number(document.getElementById("carbBudget").value)
+      ? Number(document.getElementById("carbBudget").value)
+      : null;
+    let proteinBudget = Number(document.getElementById("proteinBudget").value)
+      ? Number(document.getElementById("proteinBudget").value)
+      : null;
+
+    this.context.setUser({
+      ...this.context.user,
+      calorieBudget,
+      fatBudget,
+      carbBudget,
+      proteinBudget
+    })
+  }
+
   render() { 
     return (
       <>
@@ -28,9 +52,37 @@ class Profile extends Component {
           <input type="checkbox" id="modeToggle" onClick={this.handleModeToggle} />
           <span className="slider round"></span>
         </label>
+        <section>
+          Current Budgets
+          <br />
+          Calories: {this.context.user.calorieBudget}
+          <br />
+          Fat: {this.context.user.fatBudget}
+          <br />
+          Carbs: {this.context.user.carbBudget}
+          <br />
+          Protein: {this.context.user.proteinBudget}
+        </section>
+        <form className="userBudgets">
+          <label>
+            Calorie Budget: <input type="text" id="calorieBudget" />
+          </label>
+          <br />
+          <label>
+            Fat Budget: <input type="text" id="fatBudget" />
+          </label>
+          <br />
+          <label>
+            Carb Budget: <input type="text" id="carbBudget" />
+          </label>
+          <br />
+          <label>
+            Protein Budget: <input type="text" id="proteinBudget" />
+          </label>
+          <br />
+          <Button type="button" onClick={this.submitUserBudgets}>Submit</Button>
+        </form>
       </>
       );
   }
 }
- 
-export default Profile;
