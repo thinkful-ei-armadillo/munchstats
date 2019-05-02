5b5b0d33b2b75382ecfee1a911a30491f044c2b1
Michael Bramble <mike.w.bramble@gmail.com>
2019-05-02 17:28:42 -0400

    added logic for stuff and wrote fetch request

--- src/components/GramChart/GramChart.js
+++ src/components/GramChart/GramChart.js
@@ -11,31 +11,38 @@ chartRef = React.createRef();
 componentDidMount() {
     this.context.clearError();
 
-    let chartData = [0, 0, 0];
+    let chartData = [0, 0, 0, 0, 0, 0];
+    chartData[1] = this.context.user.carbBudget;
+    chartData[3] = this.context.user.fatBudget;
+    chartData[5] = this.context.user.proteinBudget;
     for(let i = 0; i < this.context.todayEvents.length; i++) {
         chartData[0] += this.context.todayEvents[i].carbs;
-        chartData[1] += this.context.todayEvents[i].fat;
-        chartData[2] += this.context.todayEvents[i].protein;
+        chartData[2] += this.context.todayEvents[i].fat;
+        chartData[4] += this.context.todayEvents[i].protein;
     }
     const myChartRef = this.chartRef.current.getContext("2d");
 
     new Chart(myChartRef, {
         type: 'bar',
         data: {
-            labels: ['Carbs', 'Fat', 'Protein'],
+            labels: ['Carbs', 'Carbs Goal', 'Fat', 'Fat Goal', 'Protein', 'Protein Goal'],
             datasets: [{
                 label: "Grams",
                 data: chartData,
                 backgroundColor: [
-                    // 'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.5)',
                     'rgba(54, 162, 235, 0.5)',
                     'rgba(255, 206, 86, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
                     'rgba(75, 192, 192, 0.5)'
                 ],
                 borderColor: [
-                    // 'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
                     'rgba(75, 192, 192, 1)'
                 ],
                 borderWidth: 1
--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -17,6 +17,22 @@ const AuthApiService = {
       );
   },
 
+  patchUser(user){
+    return fetch(`${config.API_ENDPOINT}/user`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+        );
+  },
+
   postLogin({ username, password }) {
     return fetch(`${config.API_ENDPOINT}/auth/token`, {
       method: 'POST',
